64e7ec915f450bd5cd695655993a07d97bf70bea
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2023-12-17 23:42:41 +0530

    fix: package name fixed
    Signed-off-by: Adithya Shetty <32545664+Maverick099@users.noreply.github.com>

--- tool/scripts/dart-coverage-helper.sh
+++ tool/scripts/dart-coverage-helper.sh
@@ -11,6 +11,6 @@ printf "//ignore_for_file: unused_import\n" >> $helper_file
 #  These lines find all the .dart file used in the project and adds them into the import lines with the package name.
 #  This ignores the .dart files created by flutter
 
-find lib '!' -path 'generated*/*' '!' -name '*.g.dart' '!' -name '*.part.dart' '!' -name '*.freezed.dart' -name '*.dart' | cut -c4- | awk -v package=web '{printf "import '\''package:%s%s'\'';\n", package, $1}' >> $helper_file
+find lib '!' -path 'generated*/*' '!' -name '*.g.dart' '!' -name '*.part.dart' '!' -name '*.freezed.dart' -name '*.dart' | cut -c4- | awk -v package=plot '{printf "import '\''package:%s%s'\'';\n", package, $1}' >> $helper_file
 
 printf "\nvoid main(){}\n" >> $helper_file
