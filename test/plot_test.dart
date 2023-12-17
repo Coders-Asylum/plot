cd389fe95f00131eff4d901a4707ed1445cb88bd
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2023-12-17 23:16:51 +0530

    patch: dummy test to pass ci

--- test/plot_test.dart
+++ test/plot_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:plot/plot.dart' show Calculator;
 
 void main() {
   test('adds one to input values', () {
+    final Calculator claculator = Calculator();
+    expect(claculator.addOne(2), 3);
   });
 }
